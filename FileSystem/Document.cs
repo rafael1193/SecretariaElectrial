1d15dd3569dabc06a5d8c56429ee4dd5221b5540
Rafael Bailón <rafaelbailon@ieee.org>
2014-02-20 19:50:20 +0100

    Fix for folder deletion erro when it's not empty

--- FileSystem/Document.cs
+++ FileSystem/Document.cs
@@ -90,8 +90,34 @@ namespace SecretariaElectrial.FileSystem
 		public void Delete()
 		{
 			string p = System.IO.Path.Combine(parentCategory.ParentRegistry.BasePath, parentCategory.GetDirectionString(), parentCategory.ToString(), ToString());
-			System.IO.Directory.Delete(p);
+			DeleteDirectory(p);
 			ParentCategory.Remove(this);
+		}
+
+		/// <summary>
+		/// Depth-first recursive delete, with handling for descendant 
+		/// directories open in Windows Explorer.
+		/// </summary>
+		public static void DeleteDirectory(string path)
+		{
+			//From http://stackoverflow.com/a/1703799
+			foreach (string directory in Directory.GetDirectories(path))
+			{
+				DeleteDirectory(directory);
+			}
+
+			try
+			{
+				Directory.Delete(path, true);
+			}
+			catch (IOException) 
+			{
+				Directory.Delete(path, true);
+			}
+			catch (UnauthorizedAccessException)
+			{
+				Directory.Delete(path, true);
+			}
 		}
 
 		/// <summary>
